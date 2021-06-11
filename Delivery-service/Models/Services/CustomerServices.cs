21871904ac35717e8011bda047a7039832abc697
Ann <kuleshowaaaaaaa@gmail.com>
2021-06-11 15:09:36 +0300

    fixed

--- Delivery-service/Models/Services/CustomerServices.cs
+++ Delivery-service/Models/Services/CustomerServices.cs
@@ -66,38 +66,18 @@ namespace DeliveryService.Services
 
         public static bool GetVerificationPhone(string number)
         {
-            var pattern = @"\+\d{3}\(\d{2}\)\d{3}\s\d{2}\s\d{2}";
-            var pattern2 = @"\+\d{12}";
-            var pattern3 = @"0\d{9}";
-            var pattern4 = @"0\d{2}\s\d{3}\s\d{2}\s\d{2}";
-
+            var pattern = @"\+?(38)?0\(?\d{2}\)?\s?\d{3}\s?\d{2}\s?\d{2}";
             var expression = new Regex(pattern, RegexOptions.Compiled);
-            var expression2 = new Regex(pattern2, RegexOptions.Compiled);
-            var expression3 = new Regex(pattern3, RegexOptions.Compiled);
-            var expression4 = new Regex(pattern4, RegexOptions.Compiled);
 
-            if (expression.IsMatch(number)
-                || expression2.IsMatch(number)
-                || expression3.IsMatch(number)
-                || expression4.IsMatch(number))
-            {
-                return true;
-            }
-
-            return false;
+            return expression.IsMatch(number);
         }
 
         public static bool GetVerificationAddress(string address)
         {
-            var pattern = @"(?:улица|ул\.?)\s?[А-Я][а-я]*\.?\,?\?:д|дом\.?\s?\d\,?\s?\?:кв|квартира.?\s?\d";
+            var pattern = @"(?:улица|ул\.?)\s?[А-Я][а-я]*\.?\,?\s?(?:д)(?:ом)?.?\s?\d*\,?\s?(?:кв)?\.?\s?(?:артира)?\s?\d*";
             var expression = new Regex(pattern, RegexOptions.Compiled);
 
-            if (expression.IsMatch(address))
-            {
-                return true;
-            }
-
-            return true;
+            return expression.IsMatch(address);
         }
     }
 }
