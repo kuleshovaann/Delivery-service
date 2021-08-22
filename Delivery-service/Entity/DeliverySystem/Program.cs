201a02da722f6621b4e51f10185230ab2991f96e
Ann <kuleshowaaaaaaa@gmail.com>
2021-08-23 00:08:03 +0300

    completed task 3 - Dapper

--- Delivery-service/Entity/DeliverySystem/Program.cs
+++ Delivery-service/Entity/DeliverySystem/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using DeliverySystem.Services;
 using DeliverySystem.DataBase;
+using DeliverySystem.Repository;
 using System.Linq;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace DeliverySystem
 {
@@ -9,12 +12,26 @@ namespace DeliverySystem
     {
         static void Main(string[] args)
         {
+            var configuration = Initialize();
+            var repository = new RepositoryProvider(configuration.GetConnectionString("DefaultConnection"));
+            var e = repository.GetProviderById(1);
+
+
             var providerDataBase = new ProviderDataBase();
             var productDataBase = new ProductDataBase();
             var creator = new DataCreator(providerDataBase, productDataBase);
             var linq = new LinqRequests(providerDataBase, productDataBase);
 
             Console.ReadKey();
-        }      
+        }
+
+        private static IConfigurationRoot Initialize()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            return builder.Build();
+        }
     }
 }
