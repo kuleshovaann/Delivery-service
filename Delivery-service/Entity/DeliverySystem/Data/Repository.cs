674b4388edff1bb6275b0f1e93e0d6148fb99212
Ann <kuleshowaaaaaaa@gmail.com>
2021-08-30 18:15:57 +0300

    Added migrations and patterns

---
+++ Delivery-service/Entity/DeliverySystem/Data/Repository.cs
@@ -0,0 +1,45 @@
+﻿using DeliverySystem.Contracts;
+using DeliverySystem.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace DeliverySystem.Data
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private DataContext _dataContext;
+
+        public Repository(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public void Create(T item)
+        {
+            _dataContext.Set<T>().Add(item);
+        }
+
+        public void Delete(int id)
+        {
+            T item = _dataContext.Set<T>().Find(id);
+            if (item != null)
+                _dataContext.Set<T>().Remove(item);
+        }
+
+        public T Get(int id)
+        {
+            return _dataContext.Set<T>().Find(id);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _dataContext.Set<T>();
+        }
+
+        public void Update(T item)
+        {
+            _dataContext.Entry(item).State = EntityState.Modified;
+        }
+    }
+}
---
+++ Delivery-service/Entity/DeliverySystem/Data/UnitOfWork.cs
@@ -0,0 +1,75 @@
+﻿using DeliverySystem.Contracts;
+using DeliverySystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliverySystem.Data
+{
+    class UnitOfWork
+    {
+        private DataContext dataContext = new DataContext();
+        private IRepository<Customer> _customerRepository;
+        private IRepository<Delivery> _deliveryRepository;
+        private IRepository<Order> _orderRepository;
+        private IRepository<Product> _productRepository;
+        private IRepository<Provider> _providerRepository;
+
+        public IRepository<Customer> Customers
+        {
+            get
+            {
+                if (_customerRepository == null)
+                    _customerRepository = new Repository<Customer>(dataContext);
+                return _customerRepository;
+            }
+        }
+
+        public IRepository<Delivery> Deliveries
+        {
+            get
+            {
+                if (_deliveryRepository == null)
+                    _deliveryRepository = new Repository<Delivery>(dataContext);
+                return _deliveryRepository;
+            }
+        }
+
+        public IRepository<Order> Orders
+        {
+            get
+            {
+                if (_orderRepository == null)
+                    _orderRepository = new Repository<Order>(dataContext);
+                return _orderRepository;
+            }
+        }
+
+        public IRepository<Product> Products
+        {
+            get
+            {
+                if (_productRepository == null)
+                    _productRepository = new Repository<Product>(dataContext);
+                return _productRepository;
+            }
+        }
+
+        public IRepository<Provider> Providers
+        {
+            get
+            {
+                if (_providerRepository == null)
+                    _providerRepository = new Repository<Provider>(dataContext);
+                return _providerRepository;
+            }
+        }
+
+        public void Save()
+        {
+            dataContext.SaveChanges();
+        }
+    }
+}
