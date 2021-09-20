d6e128683873720bcfbf4b1838360201a17f7897
kuleshovaann <82471281+kuleshovaann@users.noreply.github.com>
2021-09-20 13:41:48 +0300

    Add files via upload

---
+++ Delivery-Service.API/NewExceptionFilter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Delivery_Service.API.Filters
+{
+    public class NewExceptionFilter : IExceptionFilter
+    {
+        private readonly ILogger _logger;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public NewExceptionFilter(ILogger logger, IWebHostEnvironment webHostEnvironment)
+        {
+            _logger = logger;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public void OnException(ExceptionContext context)
+        {
+            if (_webHostEnvironment.IsDevelopment())
+            {
+                _logger.LogError($"Error - {context.Exception.Message} {context.Exception.StackTrace}");
+            }
+
+            if (_webHostEnvironment.IsEnvironment("QA"))
+            {
+                _logger.LogError($"Error - {context.Exception.Message}");
+            }
+
+            context.ExceptionHandled = true;
+        }
+    }
+}
---
+++ Delivery-Service.API/RequestBodyActionFilter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Delivery_Service.API.Filters
+{
+    public class RequestBodyActionFilter : IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            Console.WriteLine("Request body - " + context.HttpContext.Request.Body);
+        }
+    }
+}
