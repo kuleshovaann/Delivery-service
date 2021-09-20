37028754d4d0baffc9f6e8e722c7664857833d83
kuleshovaann <82471281+kuleshovaann@users.noreply.github.com>
2021-09-20 13:36:57 +0300

    Update ProductMVCController.cs

--- Delivery-Service.API/Controllers/ProductMVCController.cs
+++ Delivery-Service.API/Controllers/ProductMVCController.cs
@@ -1,10 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using DeliverySystem.DAL.Contracts;
 using DeliverySystem.DAL.Models;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Delivery_Service.API.Filters;
+using System;
 
 namespace Delivery_Service.API.Controllers
 {
+    [Route("mvc/product")]
+    [ServiceFilter(typeof(NewExceptionFilter))]
     public class ProductMVCController : Controller
     {
         private readonly IProductServices _productServices;
@@ -17,15 +21,15 @@ namespace Delivery_Service.API.Controllers
         }
 
         [HttpGet]
+        [ServiceFilter(typeof(RequestBodyActionFilter))]
         public IActionResult GetProducts()
         {
-            var products = _productServices.GetAllProducts();
-
-            return View(products);
+            return View();
         }
 
         [HttpGet]
-        public ActionResult Create()
+        [Route("Create")]
+        public IActionResult Create()
         {
             ViewBag.Providers = new SelectList(_providerServices.GetAllProviders(), "Id", "Name");
 
@@ -33,26 +37,17 @@ namespace Delivery_Service.API.Controllers
         }
 
         [HttpPost]
+        [Route("Create")]
         public IActionResult Create(Product product)
         {
-            if (!ModelState.IsValid)
-            {
-                return RedirectToAction("GetProducts");
-            }
-
             _productServices.CreateProduct(product);
 
             return RedirectToAction("GetProducts");
         }
 
         [HttpGet]
-        public IActionResult Edit()
-        {
-            return View();
-        }
-
-        [HttpGet]
-        public ActionResult Edit(int id)
+        [Route("Edit/{id}")]
+        public IActionResult Edit(int id)
         {
             Product product = _productServices.GetProductById(id);
             ViewBag.Providers = new SelectList(_providerServices.GetAllProviders(), "Id", "Name");
@@ -61,14 +56,16 @@ namespace Delivery_Service.API.Controllers
         }
 
         [HttpPost]
-        public ActionResult Edit(Product product)
+        [Route("Edit/{id}")]
+        public IActionResult Edit(Product product)
         {
             _productServices.EditProduct(product);
 
             return RedirectToAction("GetProducts");
         }
-        
-        [HttpDelete]
+
+        [HttpGet]
+        [Route("Delete/{id}")]
         public IActionResult Delete(int id)
         {
             _productServices.DeleteProduct(id);
